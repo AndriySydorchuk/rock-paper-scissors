6a1a1e4b9faf730944a99d806a908269bdbebacd
AndriySydorchuk <programmers130822@gmail.com>
2023-04-26 20:59:32 +0300

    getComputerChoice() changes: replace console.log with return; create playRound funtion, which inplements game functionality

--- script.js
+++ script.js
@@ -3,12 +3,72 @@
 function getComputerChoice() {
   const num = Math.trunc(Math.random() * 3 + 1);
   if (num === 1) {
-    console.log("Rock");
+    return "Rock";
   } else if (num === 2) {
-    console.log("Paper");
+    return "Paper";
   } else if (num === 3) {
-    console.log("Scissors");
+    return "Scissors";
   } else {
     console.log("I've no idea what to choose");
   }
 }
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Rock, Paper or Scissors?:");
+
+function playRound(playerSelection, computerSelection) {
+  console.log(playerSelection);
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  switch (playerSelection) {
+    case "rock":
+      switch (computerSelection) {
+        case "rock":
+          console.log("Draw!");
+          break;
+        case "paper":
+          console.log("You lost! Paper beats Rock");
+          break;
+        case "scissors":
+          console.log("You won! Rock beats Scissors");
+          break;
+        default:
+          console.log("Something went wrong :(");
+      }
+      break;
+    case "paper":
+      switch (computerSelection) {
+        case "rock":
+          console.log("You won! Paper beats Rock");
+          break;
+        case "paper":
+          console.log("Draw!");
+          break;
+        case "scissors":
+          console.log("You lost! Scissors beats Paper");
+          break;
+        default:
+          console.log("Something went wrong :(");
+      }
+      break;
+    case "scissors":
+      switch (computerSelection) {
+        case "rock":
+          console.log("You lost! Rock beats Scissors");
+          break;
+        case "paper":
+          console.log("You won! Scissors beats Paper");
+          break;
+        case "scissors":
+          console.log("Draw!");
+          break;
+        default:
+          console.log("Something went wrong :(");
+      }
+      break;
+    default:
+      console.log("Something wrong :(");
+  }
+}
+
+playRound(playerSelection, computerSelection);
